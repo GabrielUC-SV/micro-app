1130356daef3a57f8cfa7b0d196508615a6d55eb
GabrielUC <gabrielurias100@gmail.com>
2024-08-29 22:13:18 -0600

    first commit

---
+++ src/main/java/io/gauc/sv/micro/MicroAppApplication.java
@@ -0,0 +1,13 @@
+package io.gauc.sv.micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroAppApplication.class, args);
+	}
+
+}
